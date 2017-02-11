e518eaed4e891d5561cba757c81d11568aa505ae
Jani Eväkallio <jani.evakallio@gmail.com>
2017-02-11 23:32:11 +0000

    solution

--- server.js
+++ server.js
@@ -3,5 +3,9 @@ const statusCodes = require('./data/status-codes.json');
 const app = module.exports = express();
 
 // add your routes here :)
-
+app.get('/code/:code', (req, res) => {
+  const code = req.params.code;
+  const data = statusCodes.filter(item => item.code === code)[0];
+  res.json(data);
+})
 app.listen(8080);
