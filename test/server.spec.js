0732eff5ab37c4a9a737d71f5d2bf39103eb8091
Theo <theoleanse@gmail.com>
2017-02-12 11:51:37 +0000

    add a test for the homework (#3)

--- test/server.spec.js
+++ test/server.spec.js
@@ -5,21 +5,37 @@ const statusCodes = require('../data/status-codes.json');
 
 describe('Status codes API', () => {
 
-	it('has a \'/:code\' route', () => {
+	it('has a \'/code/:code\' route', () => {
 		return request(server)
-			.get('/200')
+			.get('/code/200')
 			.expect(200);
 	});
 
 	it('returns the correct data from the \'statusCodes\' array', () => {
 		const status = statusCodes[Math.floor(Math.random() * statusCodes.length)];
 		return request(server)
-			.get(`/${status.code}`)
+			.get(`/code/${status.code}`)
 			.expect(200)
 			.expect('Content-type', /json/)
 			.expect(res => {
 				expect(res.body).to.eql(status);
 			});
+	});
+
+	describe('Homework', () => {
+
+		it('has a \'/code\' route', () => {
+			const codesWithImages = statusCodes.map(({ code, phrase }) => (
+				{ code, phrase, image: `https://http.cat/${code}.jpg` }
+			));
+			return request(server)
+				.get('/code')
+				.expect(200)
+				.expect('Content-type', /json/)
+				.expect(res => {
+					expect(res.body).to.eql(codesWithImages);
+				});
+		});
 	});
 
 });
