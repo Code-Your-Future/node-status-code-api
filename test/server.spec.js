9cf3087a5114525c64b48a85daa80430715f6be2
TheoLeanse <theoleanse@gmail.com>
2017-02-12 08:16:02 +0000

    consistency across both practical session apps

--- test/server.spec.js
+++ test/server.spec.js
@@ -5,35 +5,21 @@ const statusCodes = require('../data/status-codes.json');
 
 describe('Status codes API', () => {
 
-	describe('GET `/code/:code`', () => {
-		it.only('should take a status code as a URL parameter and return JSON data for that status code', () => {
-			return request(server)
-				.get('/code/200')
-				.expect(200)
-				.expect('Content-type', /json/)
-				.expect(res => {
-					const code200 = statusCodes.find(status => status.code === "200");
-					expect(res.body).to.eql(code200);
-				});
-		});
-
-		it('should respond with \'404 Not Found\' if there is no such code in the data', () => {
-			return request(server)
-				.get('/code/600')
-				.expect(404);
-		});
+	it('has a \'/:code\' route', () => {
+		return request(server)
+			.get('/200')
+			.expect(200);
 	});
 
-	describe('GET `/code`', () => {
-		it('should have a GET `/code` route to provide all the status code data', () => {
-			return request(server)
-				.get('/')
-				.expect(200)
-				.expect('Content-type', /json/)
-				.expect(res => {
-					expect(res.body).to.eql(statusCodes);
-				});
-		});
+	it('returns the correct data from the \'statusCode\' array', () => {
+		const status = statusCodes[Math.floor(Math.random() * statusCodes.length)];
+		return request(server)
+			.get(`/${status.code}`)
+			.expect(200)
+			.expect('Content-type', /json/)
+			.expect(res => {
+				expect(res.body).to.eql(status);
+			});
 	});
 
 });
