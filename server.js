54e3054bd78766f9701d7fb37c8644985c02f96f
sentayhuJo <sentayhu.alim@mail.com>
2017-02-13 17:11:53 +0000

    Node version updated, chnage to ES6 syntax

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require('express');
 const statusCodes = require('./data/status-codes.json');
 
 const app = express();
+app.get('/code/:code', (req, res) => {
+  const code = req.params.code;
+  const codeResult = statusCodes.filter((statusContent) => { 
+    return statusContent.code === code;
+  });
+  if (codeResult.length === 0) {
+    res.sendStatus('404');
+  } else {
+    res.json(codeResult[0]);
+  }
+});
 
-// add your routes here :)
-
-app.listen(8080);
+app.listen(3000);
 
 module.exports = app;
