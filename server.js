8d54a4ef6aa88c0f93aa70adc0015064b2c22a84
Md-Almasri <mohieddin.almasri@hotmail.com>
2017-02-13 19:07:24 +0000

    add route /code/:code

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const statusCodes = require('./data/status-codes.json');
-
 const app = express();
 
-// add your routes here :)
+app.get('/code/:code', function (req,res) {
+	var data = statusCodes.find((value) => value.code === req.params.code)
+	res.json(data);
+});
 
 app.listen(8080);
 
