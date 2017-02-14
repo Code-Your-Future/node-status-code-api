e0d8c87c5aa356053da5b0e2aab7cfba26265d63
Angela <Baby007mix@gmail.com>
2017-02-14 19:58:11 +0000

    Work in Progress

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const statusCodes = require('./data/status-codes.json');
 
 const app = express();
 
+app.get("/code/:statusCode", function(req, res){
+	const statusCode = req.params.statusCode;
+	const codesWeWant = statusCodes.filter(item => item.code == statusCode);
+	res.json(codesWeWant[0]);
+});
+
 // add your routes here :)
 
 app.listen(8080);
