625bbea64ee24b0c74e4bd6aff23d9a7030cd11a
Md-Almasri <mohieddin.almasri@hotmail.com>
2017-02-14 15:27:17 +0000

    delete spaces

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const statusCodes = require('./data/status-codes.json');
 const app = express();
 
-app.get('/code/:code', function (req,res) {
-	var data = statusCodes.find((value) => value.code === req.params.code)
+app.get('/code/:code', function (req, res) {
+	let data = statusCodes.find((value) => value.code === req.params.code)
 	res.json(data);
 });
 
-app.get('/code', function (req,res) {
-	var data = statusCodes.map((value) => {
-		var obj = {
+app.get('/code', function (req, res) {
+	let data = statusCodes.map((value) => {
+		let obj = {
 			code: value.code,
 			phrase: value.phrase,
 			img: 'https://http.cat/' + value.code + '.jpg'
