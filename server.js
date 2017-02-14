91cd764ec6d0e92c06a95dff4d3230abe6d5a5fb
Md-Almasri <mohieddin.almasri@hotmail.com>
2017-02-14 16:29:17 +0000

    use arrow function

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const statusCodes = require('./data/status-codes.json');
+
 const app = express();
-
-app.get('/code/:code', function (req, res) {
-	let data = statusCodes.find((value) => value.code === req.params.code)
-	res.json(data);
+app.get('/code/:code', (req, res) => {
+  const data = statusCodes.find(value => value.code === req.params.code);
+  res.json(data);
 });
-
-app.get('/code', function (req, res) {
-	let data = statusCodes.map((value) => {
-		let obj = {
-			code: value.code,
-			phrase: value.phrase,
-			img: 'https://http.cat/' + value.code + '.jpg'
-		}
-		return obj;
-	});
-	res.json(data);
+app.get('/code', (req, res) => {
+  const data = statusCodes.map((value) => {
+    const obj = {
+      code: value.code,
+      phrase: value.phrase,
+      img: `https://http.cat/ ${value.code} .jpg`,
+    };
+    return obj;
+  });
+  res.json(data);
 });
 
 app.listen(8080);
