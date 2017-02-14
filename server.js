873368812af83813dee05f3463a17ea567a25257
mohibhb <mohib@InstallmacOSSierra10121-a820662a6550.local>
2017-02-14 22:24:46 +0000

    add code route

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ const express = require('express');
 const statusCodes = require('./data/status-codes.json');
 
 const app = express();
-
+app.get("/code/:code",function (req, res){
+ 	const status = req.params.code;
+ 	for(let i=0; i<statusCodes.length; i++){
+ 		if(statusCodes[i].code===status)
+ 			res.send(statusCodes[i])
+ 	}
+}) 
+app.get('/code',function (req, res){
+	const codesWithImages = statusCodes.map(({ code, phrase }) =>  ({ code, phrase, image : `https://http.cat/${code}.jpg` })); 
+	res.json(codesWithImages);
+});
 // add your routes here :)
 
-app.listen(8080);
+app.listen(8080,function (){
+	console.log("Server is findDOMNode(ReactComponent)")
+});
+
 
 module.exports = app;
