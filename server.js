696592a729e4e3aad44a5186ccd6f6d94395985e
sentayhuJo <sentayhu.alim@mail.com>
2017-02-17 23:37:31 +0000

    query sting partially started

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const express = require('express');
+const json_Query=require('json-query');
 const statusCodes = require('./data/status-codes.json');
 
 const app = express();
 
 app.get('/code/:code', (req, res) => {
-  const code = req.params.code;
+  const code = req.params.code;  
   const codeResult = statusCodes.filter((statusContent) => { 
     return statusContent.code === code;
   });
-  if (codeResult.length === 0) {
-    res.sendStatus('404');
-  } else {
-    res.json(codeResult[0]);
-  }
+  (codeResult.length === 0) ? res.sendStatus('404') : res.json(codeResult[0]);
 });
 
 app.get('/code', (req, res) => {
@@ -21,6 +18,13 @@ app.get('/code', (req, res) => {
   res.json(allCode);
 });
 
+app.get('/search/', (req, res) => {
+  const query = req.query.phrase;  
+  const codeResult = statusCodes.filter((statusContent) => { 
+    return statusContent.phrase === query;
+  });
+  (codeResult.length === 0) ? res.sendStatus('404') : res.json(codeResult[0]);
+});
 app.listen(3000);
 
 module.exports = app;
