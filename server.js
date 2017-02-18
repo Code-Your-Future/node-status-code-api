7dd72c5a91224a3211f61512e9c64c3da865e94d
sentayhuJo <sentayhu.alim@mail.com>
2017-02-18 21:36:00 +0000

    Eslint error fix, npm run ci test passed

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const express = require('express');
-const json_Query=require('json-query');
 const statusCodes = require('./data/status-codes.json');
 
 const app = express();
 
 app.get('/code/:code', (req, res) => {
-  const code = req.params.code;  
-  const codeResult = statusCodes.filter((statusContent) => { 
-    return statusContent.code === code;
-  });
-  (codeResult.length === 0) ? res.sendStatus('404') : res.json(codeResult[0]);
+  const code = req.params.code;
+  const codeResult = statusCodes.filter(statusContent => statusContent.code === code);
+  if (codeResult.length === 0) {
+    res.sendStatus('404');
+  } else {
+    res.json(codeResult[0]);
+  }
 });
 
 app.get('/code', (req, res) => {
-  const allCode = statusCodes.map(({code, phrase}) => (
-    {code, phrase, image: 'https://http.cat/' + code + '.jpg'} ));
+  const allCode = statusCodes.map(({ code, phrase }) => (
+    { code, phrase, image: `https://http.cat/${code}.jpg` }));
   res.json(allCode);
 });
 
 app.get('/search/:value', (req, res) => {
   const queryValue = req.params.value;
-  const codeResult = statusCodes.filter((statusContent) => { 
-    return statusContent.phrase.indexOf(queryValue) !== -1;
-  });  
+  const codeResult = statusCodes.filter(statusContent => (
+    statusContent.phrase.indexOf(queryValue) !== -1));
   res.json(codeResult);
 });
 
